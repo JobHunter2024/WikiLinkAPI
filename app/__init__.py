0d19564b48d25037e2bd5f0cf29ff3b28d429aa2
hartonamalia <hartonamalia99@yahoo.com>
2025-02-02 19:57:26 +0200

    added swagger

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,35 @@
 from flask import Flask
-
+from flasgger import Swagger
 
 def create_app():
     app = Flask(__name__)
+    swagger_config = {
+        "headers": [],
+        "specs": [
+            {
+                "endpoint": "apispec",
+                "route": "/apispec.json",
+                "rule_filter": lambda rule: True,
+                "model_filter": lambda tag: True,
+            }
+        ],
+        "static_url_path": "/flasgger_static",
+        "swagger_ui": True,
+        "specs_route": "/swagger/",
+    }
+
+    swagger_template = {
+        "swagger": "2.0",
+        "info": {
+            "title": "Job Hunter API",
+            "description": "API to get relevant data regarding a technology.",
+            "version": "1.0.0",
+        },
+        "host": "localhost:5001",
+        "basePath": "/",
+    }
+
+    Swagger(app, config=swagger_config, template=swagger_template)
     from app.routes.wiki_link_routes import wiki_link_bp
     app.register_blueprint(wiki_link_bp)
     return app
--- app/routes/wiki_link_routes.py
+++ app/routes/wiki_link_routes.py
@@ -1,10 +1,80 @@
 from flask import Blueprint, request, jsonify
 from app.services.wiki_link_service import get_wikidata_info, get_wikipedia_summary
+from flasgger import swag_from
 
 wiki_link_bp = Blueprint('wiki_link', __name__)
 
 
 @wiki_link_bp.route('/api/v1/wiki_link', methods=['POST'])
+@swag_from({
+    "tags": ["Wiki Data Lookup"],
+    "summary": "Fetches Wikidata and Wikipedia information for a given ontology entity.",
+    "description": "Retrieves the Wikipedia URL, Wikidata URI, logo, and a brief description for a given ontology entity.",
+    "parameters": [
+        {
+            "name": "body",
+            "in": "body",
+            "required": True,
+            "schema": {
+                "type": "object",
+                "properties": {
+                    "iri": {
+                        "type": "string",
+                        "example": "http://www.semanticweb.org/ana/ontologies/2024/10/JobHunterOntology#.NET",
+                        "description": "IRI of the ontology entity for which Wikidata and Wikipedia information is retrieved."
+                    }
+                }
+            }
+        }
+    ],
+    "responses": {
+        200: {
+            "description": "Successfully retrieved Wikidata and Wikipedia information.",
+            "schema": {
+                "type": "object",
+                "properties": {
+                    "ontology_iri": {
+                        "type": "string",
+                        "example": "http://www.semanticweb.org/ana/ontologies/2024/10/JobHunterOntology#.NET",
+                        "description": "IRI of the ontology entity."
+                    },
+                    "wikidata_uri": {
+                        "type": "string",
+                        "example": "https://en.wikipedia.org/wiki/.NET",
+                        "description": "Wikidata URI corresponding to the entity."
+                    },
+                    "wikipedia_url": {
+                        "type": "string",
+                        "example": "https://en.wikipedia.org/wiki/.NET",
+                        "description": "Wikipedia page URL related to the entity."
+                    },
+                    "logo_url": {
+                        "type": "string",
+                        "example": "http://commons.wikimedia.org/wiki/Special:FilePath/Microsoft%20.NET%20logo.svg",
+                        "description": "URL of the entity's logo from Wikidata."
+                    },
+                    "description": {
+                        "type": "string",
+                        "example": "The .NET platform is a free and open-source, managed computer software framework for Windows, Linux, and macOS operating systems. The project is mainly developed by Microsoft employees by way of the .NET Foundation and is released under an MIT License.",
+                        "description": "Brief summary of the entity extracted from Wikipedia."
+                    }
+                }
+            }
+        },
+        400: {
+            "description": "Invalid request, missing 'iri' parameter.",
+            "schema": {
+                "type": "object",
+                "properties": {
+                    "error": {
+                        "type": "string",
+                        "example": "Missing 'iri' parameter"
+                    }
+                }
+            }
+        }
+    }
+})
 def get_info():
     data = request.get_json()
     iri = data.get("iri")
